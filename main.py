a9b5cb2ef40557e88b1ccdf904f9c4d9842c4de9
zxfastxz <163340515+zxfastxz@users.noreply.github.com>
2025-04-11 16:37:48 -0300

    Aula 4

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from src.extractTransform import requestApiBcb
 from src.load import salvarCsv, salvarSQlite
 
 dadosBcb = requestApiBcb("20191")
-salvarCsv(dadosBcb, "etlBCB/src/datasets/meiosPagamentosTri.csv", ";", ".")
+# salvarCsv(dadosBcb, "etlBCB/src/datasets/meiosPagamentosTri.csv", ";", ".")
 
 salvarSQlite(dadosBcb, "src/datasets/dadosbcb.db", "meiosPagamentosTri")
 
-salvarMySQL(dadosBcb, "root", "root", "localhost", "etlbcb", "meiosPagamentostri")
+# salvarMySQL(dadosBcb, "root", "root", "localhost", "etlbcb", "meiosPagamentostri")
--- src/load.py
+++ src/load.py
@@ -4,6 +4,19 @@ from sqlalchemy import create_engine
 
 
 def salvarCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, dec: str):
+    """
+    Salva um DataFrame em um arquivo CSV.
+
+     Parâmetros:
+        df (pd.DataFrame): O DataFrame que deve ser salvo.
+        nome_arquivo (str): Caminho e nome do arquivo de saída (ex: 'dados.csv').
+        separador (str): Caractere usado como separador de colunas no CSV (ex: ',' ou ';').
+        decimal (str): Caractere usado para representar o ponto decimal (ex: '.' ou ',').
+
+    returns:
+    O DataFrame deve ser salvo em um arquivo .csv dentro da pasta de datasets.
+    """
+    # Verifica se o DataFrame está vazio
     df.to_csv(nome_arquivo, sep=separador, decimal=dec)
     return
 
